d838b1c5e2a880b18eb74ab37fec4d5a51e297ad
ocowchun <ocowchun@gmail.com>
2015-07-24 17:40:26 +0800

    add gulp to speed devlopment

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+var gulp = require('gulp');
+var gutil = require("gulp-util");
+var browserSync = require('browser-sync');
+var webpack = require("webpack");
+var webpackConfig=require('./webpack.config');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: './'
+            // ,
+            // routes: {
+            //     '/bower_components': 'bower_components'
+            // }
+        }
+    });
+});
+
+gulp.task("webpack", function(callback) {
+    // run webpack
+    webpack(webpackConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            // output options
+        }));
+        callback();
+    });
+});
+
+// Reload all Browsers
+gulp.task('bs-reload', function() {
+    browserSync.reload();
+});
+
+
+gulp.task('default', ['browser-sync'], function() {
+    gulp.watch('*.html', ['bs-reload']);
+    gulp.watch('*.css', ['bs-reload']);
+    gulp.watch('*.js', ['webpack','bs-reload']);
+
+    // body...
+});
